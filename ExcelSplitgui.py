fef73118a5907ff1418d6c18e192e1ddcc4d54aa
tnite <91829666+C1c4d4-InSec@users.noreply.github.com>
2021-12-29 12:58:08 -0600

    GUI
    Splitting accessorials into individual columns.
    File picker for ease of use.

---
+++ ExcelSplitgui.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 11:02:41 2021
+
+@author: gmahenr
+"""
+import pandas as pd
+from tkinter import filedialog as fd
+import sys
+import tkinter as tk
+
+try:
+    
+    window = tk.Tk()
+    
+    def excelfile():
+        root = tk.Tk()
+        root.withdraw()
+        
+        try:
+            file_path = fd.askopenfilename()
+        except Exception as err:
+            sys.exit('[-] Something went wrong:', err)
+            
+        root.update() 
+        # Take opened file and import as dataframe in Pandas.
+        try:
+            file = pd.ExcelFile(file_path)
+        except Exception:
+            sys.exit('[-] Something went wrong:')
+            
+        # Open the first sheet in the file.
+        df = file.parse(sheet_name=0)
+        
+        # CLose the file, free up memory.
+        file.close()
+        
+        # Grab the column names in the file.
+        column_list = list(df)
+        
+        # Commence splitting apart the billcodes and costs.
+        df[column_list[1]] = df[column_list[1]].str.split(',')
+        
+        # Fill in NaNs with 0s
+        df.fillna(0, inplace=True)
+        
+        # Make the DF a list; easier to work with for me.
+        zip1 = zip(df[column_list[0]], df[column_list[1]])
+        list2 = list(zip1)
+        
+        # Get rid of excess spaces.
+        for item in list2:
+            if item[1] == 0:
+                continue
+            else:
+                for i in range(0, len(item[1])):
+                     item[1][i] = item[1][i].split(';')
+                     item[1][i][1] = item[1][i][1].strip()
+        
+        # Get a list of unique bill codes.
+        codeList = ''
+             
+        for key, item in list2:
+            if item == 0:
+                continue
+            else:
+                for x in item:
+                    for i in range(0,1):
+                        codeList += x[i].strip() + ','
+        
+        # Full clean split list of bill codes.            
+        codeList = codeList.split(',')
+        
+        # Make a list for the unique bill codes.
+        uniqueCodes = []
+        
+        # Fill the blank list with the unique codes, sort them,
+        # and remove the empty columns as necessary.
+        for i in range (0, len(codeList)):
+            if codeList[i] not in uniqueCodes:
+                uniqueCodes.append(codeList[i])        
+           
+        uniqueCodes.sort()
+        uniqueCodes.remove('')
+        
+        # Create list of pros.
+        proList = []
+        for item in df[column_list[0]]:
+            proList.append(item)
+        
+        # Create new data frame with columns of the unique bill codes.
+        df2 = pd.DataFrame([],
+                           index=[proList],
+                           columns=[uniqueCodes])
+        
+        # Import costs from first DF, add to columns corresponding to pronumber.
+        for item in list2:
+            if item[1] == 0:
+                continue
+            else:
+                for x in item[1]:
+                    #print(item[0])
+                    df2.loc[[item[0]], x[0].strip()] = x[1]
+        
+        # Add 0 to all empty cells.        
+        df2.fillna(0, inplace=True)
+        
+        # Extract original filename then save the new DF as an excel file.
+        filesave = file_path.split('/')
+        filesave = filesave[-1].split('.')
+        filesave = filesave[0]
+        filesave += '_split.xlsx'
+        
+        try:
+            df2.to_excel('../Desktop/' + filesave)
+        except Exception as err:
+            sys.exit('[-] Something went wrong:', err)
+            
+    excelSplitButton = tk.Button(text='Excel Split', width=15, height=2, command=excelfile)
+    
+    excelSplitButton.pack()
+    
+    excelSplitButton.mainloop()
+    
+except KeyboardInterrupt:
+    print("Exiting")
